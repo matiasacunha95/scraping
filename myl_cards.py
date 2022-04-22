0021cb7ae06dc188c858666fe9ad2c0d4e9ddf5b
matiasacunha95 <matiasacunha95@gmail.com>
2022-04-22 16:15:15 -0400

    [ADD] configs getter

--- myl_cards.py
+++ myl_cards.py
@@ -1,4 +1,4 @@
-''' Script that obtains all myl cards by edition '''
+''' Script that obtains all myl PB cards by edition '''
 
 import json
 import requests
@@ -10,15 +10,26 @@ def export_data_to_file(filename, data):
     with open(f'{filename}.json', 'w') as outfile:
         json.dump(data, outfile, indent=4, ensure_ascii=False)
 
-def get_data(edition):
-    #get data from request and build a json object
+def get_configs(edition):
+    #Get data from request and build a json object
     data = requests.get(base_api_url + edition)
     data = json.loads(data.content)
-    print(data)
-    export_data_to_file(edition, data)
+    export_data_to_file('races', data['races'])
+    export_data_to_file('rarities', data['rarities'])
+    export_data_to_file('types', data['types'])
+    export_data_to_file('keywords', data['keywords'])
+
+def get_data(edition):
+    #Get data from request and build a json object
+    data = requests.get(base_api_url + edition)
+    data = json.loads(data.content)
+    export_data_to_file(edition + '-cards', data['cards'])
+    export_data_to_file(edition, data['edition'])
 
 def main():
     editions_data = []
+    get_configs('undefined') 
+
     for edition in editions:
         get_data(edition)
    
